8d5b27cbcebcfacc96f9c0af58582162165ec420
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-11-18 12:39:41 +0100

    Fix case study carousel

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation();
+
+
+$(document).ready(function(){
+    $('#casestudy--carousel').slick({
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="assets/img/aetm-arrow-white-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="assets/img/aetm-arrow-white-right.svg"></button>',
+        asNavFor: '#casestudy--carousel-text'
+    });
+
+    $('#casestudy--carousel-text').slick({
+        arrows: false,
+        adaptiveHeight: true
+    });
+});
