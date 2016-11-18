205b326c0951f1e93e4ac5bc90d336632db1d61e
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-11-18 14:44:35 +0100

    Implements products carousel

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -13,4 +13,16 @@ $(document).ready(function(){
         arrows: false,
         adaptiveHeight: true
     });
+
+    $('#methods--carousel').slick({
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="assets/img/aetm-arrow-yellow-left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="assets/img/aetm-arrow-yellow-right.svg"></button>',
+        asNavFor: '#methods--carousel-text'
+    });
+
+    $('#methods--carousel-text').slick({
+        arrows: false,
+        adaptiveHeight: true
+    });
 });
