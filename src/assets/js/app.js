684a01f50e885f63c63cd5a2963c0ea5946ca61f
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-11-23 23:36:35 +0100

    Reorder first slider and removes responsive height

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,14 +3,12 @@ $(document).foundation();
 $(document).ready(function () {
     // Case study section
     $('#casestudy--carousel').slick({
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="assets/img/aetm-arrow-white-left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="assets/img/aetm-arrow-white-right.svg"></button>',
         asNavFor: '#casestudy--carousel-text'
     });
     $('#casestudy--carousel-text').slick({
         arrows: false,
-        adaptiveHeight: true,
         asNavFor: '#casestudy--carousel'
     });
 
