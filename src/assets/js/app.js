20cf5d65390781bb89204621f562a7ca486f60f7
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2016-11-23 23:26:24 +0100

    Fix vertical height for some sliders

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,5 @@
 $(document).foundation();
 
-
 $(document).ready(function () {
     // Case study section
     $('#casestudy--carousel').slick({
@@ -17,23 +16,19 @@ $(document).ready(function () {
 
     // "Did you know ?" section
     $('#didyouknow--carousel').slick({
-        // autoplay: true,
         initialSlide: Math.floor((Math.random() * 7) + 1),
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="assets/img/aetm-arrow-white-left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="assets/img/aetm-arrow-white-right.svg"></button>'
     });
 
     // Methods section
     $('#methods--carousel').slick({
-        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="assets/img/aetm-arrow-yellow-left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="assets/img/aetm-arrow-yellow-right.svg"></button>',
         asNavFor: '#methods--carousel-text'
     });
     $('#methods--carousel-text').slick({
         arrows: false,
-        adaptiveHeight: true,
         asNavFor: '#methods--carousel'
     });
 });
